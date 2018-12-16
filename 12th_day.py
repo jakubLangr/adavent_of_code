78a18b295806ebb162dbaed0a1a0331f10cc05c4
jakubLangr <james.langr@gmail.com>
2018-12-16 22:07:35 +0000

    Probably a dead end in day 12

--- 12th_day.py
+++ 12th_day.py
@@ -39,8 +39,10 @@ def find(s, ch="#"):
 mapped_repr = find(init_state)
 
 
-def evolve(init_state: str, rules: dict):
-    init_state = '00' + init_state + '00'
+def evolve(init_state: str, rules: dict, offset: int = 0):
+    if '#' in init_state[:3]:
+        init_state = '00' + init_state + '00'
+        offset += 1
     next_state = ''
     for i, p in enumerate(init_state):
         if i>0:
@@ -53,15 +55,15 @@ def evolve(init_state: str, rules: dict):
                 next_state += '.'
     
     next_state = next_state.replace('0','')
-    return next_state[0:-1]
+    return next_state, offset
 
 
 target_s1 = '.#...#....#.....#..#..#..#'
-target_s3 = '...#.#...#..#.#....#..#..#...#'.ljust(35, '.')
-s1 = evolve(init_state, rules_dict)
-s2 = evolve(s1, rules_dict)
-s3 = evolve(s2, rules_dict)
-print(s3.rjust(35, '.'))
+target_s3 = '...#.#...#..#.#....#..#..#...#'
+s1, o = evolve(init_state, rules_dict)
+s2, o = evolve(s1, rules_dict, o)
+s3, o = evolve(s2, rules_dict, o)
+print(s3)
 print(target_s3)
 print('S1 : ', find(s1, '#'))
 print('Target S1 : ', find(target_s1,'#'))
@@ -69,29 +71,33 @@ print('Target S1 : ', find(target_s1,'#'))
                            
 assert find(target_s1) == find(s1)
 
-next_step = init_state
-for i in range(20):
-    next_step = evolve(next_step, rules_dict)
+def part_one(init_state, rules, n_evol=20):
+    offset = 0
+    full_rules = {}
+    for r in rules:
+        p,r = r.split(' => ')
+        full_rules[p] = r
     
-ans = [x - 20 for x in find(next_step) ]
-print(sum(ans))
+    next_step = init_state
+    for i in range(n_evol):
+        next_step, offset = evolve(next_step, full_rules, offset)
+        
+    ans = [x for x in find(next_step) ]
+    return sum(ans)
 
+
+
+test_ans = part_one(init_state, rules)
+print(test_ans)
+# assert test_ans==325
 
 rules = open('day12_rules_input.txt').read().split('\n')
-full_rules = {}
-for r in rules:
-    p,r = r.split(' => ')
-    full_rules[p] = r
 
-next_step = working_state
-for i in range(20):
-    next_step = evolve(next_step, full_rules)
-    # print(find(next_step[i:-i]))
-    
-ans = [x - 20 for x in find(next_step) ]
-print(sum(ans))
-assert sum(ans)==3061
+ans = part_one(working_state, rules)
+print(ans)
+assert ans==3061
 
 prev_guesses = [705, 3059, 3115, 3003] # 3115 too high 
 
-
+ans = part_one(working_state, rules, 15000)
+print(ans)
